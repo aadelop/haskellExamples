710f87af2089214717e5226cb294a86c0982962d
aadelop <10-10195@usb.ve>
2016-10-23 19:03:22 -0400

    agregadas nuevas funciones hagenda

--- hagenda.hs
+++ hagenda.hs
@@ -12,8 +12,34 @@ x :: Picture
 y :: Picture
 a :: Picture
 b :: Picture
-
 (x,y,a,b) = (pixel 'x',pixel 'y',pixel 'a',pixel 'b')
+
+type Day = Int -- Suponga que está entre 1 y 31
+type Year = Int -- Suponga que es positivo
+data Month = Enero
+    | Febrero
+    | Marzo
+    | Abril
+    | Mayo
+    | Junio
+    | Julio
+    | Agosto
+    | Septiembre
+    | Octubre
+    | Noviembre
+    | Diciembre
+    deriving (Show,Eq,Ord,Enum)
+
+data DayName = Domingo
+    | Lunes
+    | Martes
+    | Miercoles
+    | Jueves
+    | Viernes
+    | Sabado
+    deriving (Show,Eq,Ord,Enum)
+
+
 pixel :: Char -> Picture
 pixel c = Picture{
         height = 1,
@@ -46,7 +72,8 @@ beside p0 p1
            (pp0, pp1) = (pixels p0, pixels p1)
 
 toString :: Picture -> String 
-toString p  =  (foldr (++) "" (pixels p)
+toString  =  (unlines .pixels) 
+
 
 stack:: [Picture] -> Picture
 stack = foldr1 above
@@ -58,15 +85,57 @@ spread :: [Picture] -> Picture
 spread = foldr1 beside
 
 row :: String -> Picture
-row  s = spread (map pixel s) 
+row   = (spread . (map pixel)) 
 
-blank :: (Height, Width) -> Picture
-blank (h,w) = let stackB h = stack (replicate h (pixel ' '))
-              in Picture {
-                    height = h,
-                    width = w,
-                    pixels = pixels (spread (replicate w (stackB h)))
-                }
+blank::(Height, Width) -> Picture
+blank = uncurry buildBlank 
+         where stackB h = stack (replicate h (pixel ' '))
+               buildBlank h w = spread (replicate w (stackB h))
 
-stackWith :: Height -> [Picture] -> Picture
-stackWith h ps = undefined
+--stackWith :: Height -> [Picture] -> Picture 
+--stackWith h ps = stack (head ps : (tail (map (above (conWhite h)) ps)))
+--                 where conWhite h = blank (h,width (head ps))
+
+stackWith :: Height -> [Picture] -> Picture 
+stackWith h = (stack . (map (buildBlank h)))
+               where buildBlank h ps = above ps (blank (h,width ps))
+
+
+stackWith' :: Height -> [Picture] -> Picture 
+stackWith' h = foldr1 (\ps -> above (above ps (blank (h, width ps))))
+
+spreadWith :: Height -> [Picture] -> Picture
+spreadWith w =(spread . (map (buildBlank w )))
+               where buildBlank w hs = beside hs (blank(height hs,w))
+
+
+tile :: [[Picture]] -> Picture
+tile = (spread . (map stack)) 
+
+tileWith::(Height, Width) -> [[Picture]] -> Picture
+tileWith (h,w) = ((spreadWith w). (map (stackWith h)))
+
+
+{-CALENDAR TYPE-}
+
+leap :: Year -> Bool
+leap y = if (((mod y 4 ==0) && 
+            (mod y 100 /= 0)) ||
+            (mod y 400 == 0))         
+         then True
+         else False
+
+mlengths :: Year -> [Day]
+mlengths y = [31,feb,31,30,31,30,31,31,30,31,30,31]
+        where feb | leap y = 29
+                      | otherwise = 28
+
+jan1 :: Year -> DayName
+jan1 y =  toEnum (((numDays y) `mod` 7))
+        where numDays y  = (y-1) * 365 + numLeap y + 1 
+              numLeap y  = length (filter leap [1..y-1])
+
+mtotals :: Year -> [Int]
+mtotals y =scanl (+) (fromEnum (jan1 y)) (mlengths y)
+
+
